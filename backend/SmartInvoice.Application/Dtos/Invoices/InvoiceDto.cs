2c8fe1bb65034d1d1f35f4b5eadd922f5b033e66
Raudy <Raudy206@gmail.com>
2025-11-05 19:25:05 -0400

    style: corrige la identacion

--- backend/SmartInvoice.Application/Dtos/Invoices/InvoiceDto.cs
+++ backend/SmartInvoice.Application/Dtos/Invoices/InvoiceDto.cs
@@ -4,15 +4,15 @@ using SmartInvoice.Application.Dtos;
 public class InvoiceDto
 {
     public int Id { get; set; }
-        public string ClientName { get; set; }
-        public string InvoiceNumber { get; set; }
-        public DateTime IssueDate { get; set; }
-        public DateTime DueDate { get; set; }
-        public string Status { get; set; }
-        public decimal SubTotal { get; set; }
-        public decimal TaxTotal { get; set; }
-        public decimal Discount { get; set; }
-        public decimal Total { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public List<InvoiceItemDto> InvoiceItems { get; set; }
+    public string ClientName { get; set; }
+    public string InvoiceNumber { get; set; }
+    public DateTime IssueDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public string Status { get; set; }
+    public decimal SubTotal { get; set; }
+    public decimal TaxTotal { get; set; }
+    public decimal Discount { get; set; }
+    public decimal Total { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public List<InvoiceItemDto> InvoiceItems { get; set; }
 }
